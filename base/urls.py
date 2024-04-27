80a5c30a8a9d052efd5e4b6220d790ff102a1eef
rociotatiana <rtpereza@gmail.com>
2024-04-27 01:06:12 -0400

    improved delete method

--- base/urls.py
+++ base/urls.py
@@ -8,11 +8,12 @@ urlpatterns = [
     path('perfil', views.perfil, name = "perfil"),
     path('comunidades', views.comunidades, name = "comunidades"),
     path('ingresar_beneficiaria', views.ingresar_beneficiaria, name = "ingresar_beneficiaria"),
+    path('tus_registros', views.tus_registros, name = "tus_registros"),
     path('registro_intervencion', views.registro_intervencion, name = "registro_intervencion"),
     path('ingresar_intervencion', views.ingresar_intervencion, name = "ingresar_intervencion"),
-    path('intervencion_form', views.intervencion_form, name = "intervencion_form"),
     path('actualizar_intervencion/<str:pk>', views.actualizar_intervencion, name = "actualizar_intervencion"),
     path('ingresar_derivacion', views.ingresar_derivacion, name = "ingresar_derivacion"),
+    path('eliminar_intervencion/<str:pk>', views.eliminarIntervencion, name = 'eliminar_intervencion')
 
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,16 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 # Create your views here.
 from django.db.models import Q
 from .models import User, Comunidad, Registro_Intervencion
 from .forms import IntervencionForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return render(request, 'base/home.html')
 
-def loginPage(request):
 
+def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -31,72 +33,83 @@ def loginPage(request):
     context = {}
     return render(request, 'base/login_register.html', context)
 
+
 def logoutUser(request):
     logout(request)
     return redirect ('home')
 
+
+@login_required(login_url='login')
 def perfil(request):
     return render(request, 'base/perfil.html')
 
+
+@login_required(login_url='login')
 def comunidades(request):
     comunidades = Comunidad.objects.all()
     context = {'comunidades': comunidades}
     return render(request, 'base/comunidades.html', context)
 
+
+@login_required(login_url='login')
 def ingresar_beneficiaria(request):
     return render(request, 'base/ingresar_beneficiaria.html')
 
-def ingresar_intervencion(request):
-    return render(request, 'base/ingresar_intervencion.html')
 
-def intervencion_form(request):
+@login_required(login_url='login')
+def tus_registros(request):
+    registros = Registro_Intervencion.objects.all()
+    context = {'registros': registros}
+    return render(request, 'base/tus_registros.html', context)
+
+
+@login_required(login_url='login')
+def ingresar_intervencion(request):
     form = IntervencionForm()
     if request.method == 'POST':
         form = IntervencionForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
-        else:
-            messages.error(request, 'Hay algo malo en el formulario')
 
     context = {'form': form}
-    return render(request, 'intervencion_form.html', context)
+    return render(request, 'base/intervencion_form.html', context)
 
+
+@login_required(login_url='login')
 def actualizar_intervencion(request, pk):
     reg_intervencion = Registro_Intervencion.objects.get(id=pk)
     form = IntervencionForm(instance=reg_intervencion)
+
+    if request.user != reg_intervencion.user:
+        return HttpResponse("No tienes permitida esta acción")
     
     if request.method == 'POST':
         form = IntervencionForm(request.POST, instance= reg_intervencion)
         if form.is_valid():
             form.save()
-            return redirect('base/home')
+            return redirect('home')
     context = {'form': form}
-    return render(request, 'intervencion_form.html', context)
+    return render(request, 'base/intervencion_form.html', context)
 
+
+@login_required(login_url='login')
 def registro_intervencion(request, pk):
     registros = Registro_Intervencion.objects.get(pk=id)
     context = {'registros': registros}
     return render(request, 'base/registro_intervencion.html', context)
 
+
+@login_required(login_url='login')
 def ingresar_derivacion(request):
     return render(request, 'base/ingresar_derivacion.html')
 
-# COMUNIDADES:
-# X Crear comunidades 
 
-## BENEFICIARIAS:
-# X Crear nueva beneficiaria
+@login_required(login_url='login')
+def eliminarIntervencion(request, pk):
+    reg_intervencion = Registro_Intervencion.objects.get(id=pk)
+    if request.method == 'POST':
+        reg_intervencion.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': reg_intervencion})
 
-
-## PROGRAMA
-# Incorporar nuevo programa
-
-# REGISTRO DE INTERVENCION
-# Crear RI
-# Actualizar RI
-
-# PLANILLA DE DERIVACIÓN
-# Crear planilla
-
-# INDICADORES
